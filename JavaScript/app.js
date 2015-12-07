b5a4fd3785ce08c0d56f372e2f735e516ff1bad7
blkrice <blkrice123@gmail.com>
2015-12-07 21:18:44 +0900

    13 - Comparisons

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,2 +1,7 @@
-console.log(2 * 5);
-console.log("Hello");
+// Here is an example of using the greater than (>) operator.
+console.log(15 > 4); // 15 > 4 evaluates to true, so true is printed.
+
+// Fill in with >, <, === so that the following print out true:
+console.log("Xiao Hui".length < 122);
+console.log("Goody Donaldson".length > 8);
+console.log(8*2 === 16);
