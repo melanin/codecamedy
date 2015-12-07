26dbf703a79bbddba95398a3df4dad54ed4f3b74
blkrice <blkrice123@gmail.com>
2015-12-07 21:30:41 +0900

    18 - Mid-lesson breather

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,12 +1,7 @@
-// The computer doesn't worry about extra spaces between words or brackets
-// It just cares about the order of where things are placed
-// and that you have used the right characters (){}[]"";
+// This is an example of an if / else statement.
 
-// For extra help, a program called a 'linter' is checking your code
-// and will put a red 'x' next to the first line that contains errors
-
-if (10 === 10) {
-    console.log("You got a true!");
+if (12 / 4 === "Ari".length) {
+    confirm("Will this run the first block?");
 } else {
-    console.log("You got a false!");
+    confirm("Or the second block?");
 }
