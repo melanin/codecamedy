932f0e489131723a03017676ba185cb061405c76
blkrice <blkrice123@gmail.com>
2015-12-07 20:40:10 +0900

    08 - Interactive JavaScript

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,1 +1,3 @@
-confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');
+// Also try the Q&A forum to get help
+// The link is on the bottom left of the page!
+confirm('확인창을 띄웁니다.');
