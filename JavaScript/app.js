56ab5fc3c2277514d8cab4fdf2dc469a3d2d185f
blkrice <blkrice123@gmail.com>
2015-12-08 20:37:43 +0900

    26 - Change variable values

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,9 @@
-// Declare a variable on line 3 called
-// myCountry and give it a string value.
-var myCountry = "Korea";
-
-// Use console.log to print out the length of the variable myCountry.
-console.log(myCountry.length);
-
-// Use console.log to print out the first three letters of myCountry.
-console.log(myCountry.substring(0, 3));
+// On line 2, declare a variable myName and give it your name.
+var myName = "MELA";
+// On line 4, use console.log to print out the myName variable.
+console.log(myName);
+// On line 7, change the value of myName to be just the first 2 
+// letters of your name.
+myName = myName.substring(0, 2);
+// On line 9, use console.log to print out the myName variable.
+console.log(myName);
