cb851b596ad0ca31b7f8f322998ab1c776bbc1e4
blkrice <blkrice123@gmail.com>
2015-12-08 23:31:36 +0900

    12 - Functions & if / else

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,6 +1,18 @@
-var nameString = function (name) {
-	
-	return "Hi, I am" + " " + name;
-};
+// Write your function below. 
+// Don't forget to call your function!
 
-console.log(nameString("MELA"));
+var sleepCheck = function(numHours)
+{
+    if(8 <= numHours)
+    {
+        return "You're getting plenty of sleep! Maybe even too much!";
+    }
+    else
+    {
+        return "Get some more shut eye!";
+    }
+}
+
+console.log(sleepCheck(10));
+console.log(sleepCheck(5));
+console.log(sleepCheck(8));
