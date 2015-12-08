752ad723c73e6cf9c29c88f3527bcca225d29569
blkrice <blkrice123@gmail.com>
2015-12-08 23:44:52 +0900

    03 - Computer Choice: Part 1

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,2 +1,5 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-console.log(userChoice);
+//var userChoice = prompt("Do you choose rock, paper or scissors?");
+//console.log(userChoice);
+
+var computerChoice = Math.random();
+console.log(computerChoice);
