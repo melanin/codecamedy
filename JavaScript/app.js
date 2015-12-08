2c280c94db974da31ff89db13a6910da1305c046
blkrice <blkrice123@gmail.com>
2015-12-08 23:37:53 +0900

    01 - 01 - The Game ( Lesson: Build "Rock, Paper, Scissors" )

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,18 +1,1 @@
-// Write your function below. 
-// Don't forget to call your function!
-
-var sleepCheck = function(numHours)
-{
-    if(8 <= numHours)
-    {
-        return "You're getting plenty of sleep! Maybe even too much!";
-    }
-    else
-    {
-        return "Get some more shut eye!";
-    }
-}
-
-console.log(sleepCheck(10));
-console.log(sleepCheck(5));
-console.log(sleepCheck(8));
+//The Game
