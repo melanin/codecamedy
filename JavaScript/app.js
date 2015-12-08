1f2a8a5319f92de70a5f767bafd64d9ea29534fd
blkrice <blkrice123@gmail.com>
2015-12-08 21:28:54 +0900

    05 - The story heats up!

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,16 @@
 var introduction = "You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'";
-
 console.log(introduction);
 
 console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
 var userAnswer = prompt("Do you want to race Bieber on stage?");
 console.log(userAnswer);
+
+if("yes" === userAnswer)
+{
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+}
+else
+{ // Otherwise...
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+}
