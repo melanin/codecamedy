2f12a3693b322bb72adb057952514799bee3ce23
blkrice <blkrice123@gmail.com>
2015-12-08 23:25:47 +0900

    11 - Functions recap

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,13 +1,6 @@
-var my_number = 7; //this has global scope
+var nameString = function (name) {
+	
+	return "Hi, I am" + " " + name;
+};
 
-var timesTwo = function(number) {
-    my_number = number * 2;
-    // var my_number = number * 2;
-    console.log("Inside the function my_number is: ");
-    console.log(my_number);
-}; 
-
-timesTwo(7);
-
-console.log("Outside the function my_number is: ")
-console.log(my_number);
+console.log(nameString("MELA"));
