48e4f72115ff4eaf2c54c6e8dd831a4d0646fb16
blkrice <blkrice123@gmail.com>
2015-12-08 23:54:09 +0900

    05 - Both choices are the same!

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,18 +1,17 @@
-//var userChoice = prompt("Do you choose rock, paper or scissors?");
-//console.log(userChoice);
-
+/*var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
-console.log(computerChoice);
-if(0.33 > computerChoice)
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);*/
+
+var compare = function(choice1, choice2)
 {
-    computerChoice = "rock";
+    if(choice1 === choice2)
+    {
+        return "The result is a tie!";
+    }
 }
-else if(0.66 > computerChoice)
-{
-    computerChoice = "paper";
-}
-else
-{
-    computerChoice = "scissors";
-}
-console.log(computerChoice);
