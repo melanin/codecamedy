c4baa71e9ffd8e990fa7e0564cc9e4db55a066c7
blkrice <blkrice123@gmail.com>
2015-12-08 21:02:02 +0900

    04 - First move!

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,7 @@
 var introduction = "You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'";
 
 console.log(introduction);
+
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+var userAnswer = prompt("Do you want to race Bieber on stage?");
+console.log(userAnswer);
