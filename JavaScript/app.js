a11641c0e4457e37426f8b9c76f43e9d36f8e9a2
blkrice <blkrice123@gmail.com>
2015-12-08 20:55:21 +0900

    02 - Old enough to play?

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,13 @@
 // Check if the user is ready to play!
 
-confirm("I am ready to play!");
+// confirm("I am ready to play!");
+
+var age = prompt("몇살이고?");
+if(13 < age)
+{
+    console.log("마이 무겄네");
+}
+else
+{
+    console.log("엄마 젖먹고 와라");
+}
