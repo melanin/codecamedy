e70ac42ad37ce0183df636105d0c05b1ed5add6d
blkrice <blkrice123@gmail.com>
2015-12-09 17:58:30 +0900

    02 - If / else if / else

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,12 +1,20 @@
 var isEven = function(number) {
-  // Your code goes here!
-  
-  if(0 == number % 2)
-  {
-      return true;
-  }
-  else
-  {
-      return false;
-  }
+// Your code goes here!
+
+    if(0 == number % 2)
+    {
+        return true;
+    }
+    else if(isNaN(number))
+    {
+        return "please input number";
+    }
+    else
+    {
+        return false;
+    }
 };
+
+console.log(isEven(100));
+console.log(isEven("test"));
+console.log(isEven(21));
