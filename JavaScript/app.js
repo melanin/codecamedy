f8a1061f883c06adfafc97076b50368d6a0a25e0
blkrice <blkrice123@gmail.com>
2015-12-09 14:00:17 +0900

    01 - What you'll be building ( Lesson: Dragon Slayer! )

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,22 +1,23 @@
-// Write your code below!
+var slaying = true;
+// A bit of new math magic to calculate the odds
+// of hitting the dragon. We'll cover this soon!
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
+var totalDamage = 0;
 
-var sum = 0;
-for(var i=1; i<=5; i++)
-{
-    sum += i;
+while (slaying) {
+  if (youHit) {
+    console.log("You hit the dragon and did " + damageThisRound + " damage!");
+    totalDamage += damageThisRound;
+    
+    if (totalDamage >= 4) {
+      console.log("You did it! You slew the dragon!");
+      slaying = false;
+    } else {
+      youHit = Math.floor(Math.random() * 2);
+    }
+  } else {
+    console.log("The dragon burninates you! You're toast.");
+    slaying = false;
+  }
 }
-console.log("SUM = " + sum);
-
-var count1 = 3;
-while(count1)
-{
-    console.log("while LOOP");
-    count1--;
-}
-
-var count2 = 2;
-do
-{
-    console.log("do - while LOOP");
-    count2--;
-}while(count2);
