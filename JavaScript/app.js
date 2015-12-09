65a219139dc994008d12259f27232228f787105f
blkrice <blkrice123@gmail.com>
2015-12-09 13:52:14 +0900

    10 - To learn it, you gotta 'do' it

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,9 @@
-var loopCondition = false;
+var getToDaChoppa = function(){
+  // Write your do/while loop here!
+  do
+  {
+      console.log("do - while LOOP");
+  } while(false);
+};
 
-do {
-	console.log("I'm gonna stop looping 'cause my condition is " + loopCondition + "!");	
-} while (loopCondition);
+getToDaChoppa();
