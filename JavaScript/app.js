0355984c17e9e8bcfc02fcd692af9d5d67253d52
blkrice <blkrice123@gmail.com>
2015-12-09 21:24:02 +0900

    04 - Switch

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,21 @@
 var user = prompt("?", "??").toUpperCase();
 //var user = prompt("?", "??").toLowerCase();
 console.log(user);
+
+switch(user)
+{
+    case "BANANA":
+        console.log("반하나");
+        break;
+        
+    case "APPLE":
+        console.log("사과하나");
+        break;
+        
+    case "ORANGE":
+        console.log("오륀지");
+        break;
+        
+    default:
+        console.log("그럽디까");
+}
