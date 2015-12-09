665bda26f0139410a21152d331dd2259b0c2bc1f
blkrice <blkrice123@gmail.com>
2015-12-09 16:50:00 +0900

    03 - The 'while' loop

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -3,4 +3,7 @@ var youHit = Math.floor(Math.random() * 2);
 var damageThisRound = Math.floor(Math.random() * 5 + 1);
 var totalDamage = 0;
 
-console.log("dragon attack(" + Math.random() * 2 + "), " + "player attack(" + Math.random() * 5 + 1 + ")");
+while(slaying)
+{
+    slaying = false;
+}
