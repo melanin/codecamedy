16ac12c44f27665442cd7169250c609213e0f2ab
blkrice <blkrice123@gmail.com>
2015-12-09 11:45:25 +0900

    02 - First for loop

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,5 @@
-// Write five console.log statements.
+// Example of a for loop:
 
-console.log(1);
-console.log(2);
-console.log(3);
-console.log(4);
-console.log(5);
+for (var counter = 1; counter < 11; counter++) {
+	console.log(counter);
+}
