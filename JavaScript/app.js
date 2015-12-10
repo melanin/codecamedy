a1bf4f8006db1e75da6998387791d4241de916d8
blkrice <blkrice123@gmail.com>
2015-12-10 23:00:13 +0900

    16 - Editing an existing object

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,6 @@
-var newArray =
-[
-    [1, [ 10, 3 ], 2], [7]
-];
-
-for(var i=0; i<newArray.length; i++)
-{
-    console.log(newArray[i]);
-}
+var myObject = {
+  name: 'Eduardo',
+  type: 'Most excellent',
+  // Add your code here!
+  interests: [ 10, "abc" ]
+};
