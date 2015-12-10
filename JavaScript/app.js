d8d7bfb002edda8bee4922435c97e170dbd89dbc
blkrice <blkrice123@gmail.com>
2015-12-10 22:10:58 +0900

    14 - Heterogeneous arrays

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,16 +1,9 @@
-var object1 = new Object();
-var object2 = new Object();
-var object3 = new Object();
+var myArray =
+[
+    12, false, "abc", [ 7, "def" ]
+];
 
-object1.name = "abc";
-object1.age = 12;
-
-object2.strValue = "sss";
-object2.numValue = 22;
-
-object3.boolean = false;
-object3.count = 12;
-
-console.log(object1);
-console.log(object2);
-console.log(object3);
+for(var i=0; i<myArray.length; i++)
+{
+    console.log(myArray[i]);
+}
