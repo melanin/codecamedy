38b109d2b25a49ac92962f75701f739aac2ffa5d
blkrice <blkrice123@gmail.com>
2015-12-10 23:24:25 +0900

    05 - Tossing in an array

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -5,6 +5,7 @@ friends.bill =
     firstName: "Bill"
     , lastName: "Gates"
     , number: 12
+    , address: ["abcdef", "ghi"]
 };
 
 friends.steve =
@@ -12,6 +13,7 @@ friends.steve =
     firstName: "Steve"
     , lastName: "Jobs"
     , number: 25
+    , address: ["jklmn", "opq"]
 };
 
 console.log(friends);
