a4ce39caaee74928f15f342bbb99d9246e68561c
blkrice <blkrice123@gmail.com>
2015-12-10 22:17:55 +0900

    15 - Multidimensional arrays

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,9 @@
-var myArray =
+var newArray =
 [
-    12, false, "abc", [ 7, "def" ]
+    [1, [ 10, 3 ], 2], [7]
 ];
 
-for(var i=0; i<myArray.length; i++)
+for(var i=0; i<newArray.length; i++)
 {
-    console.log(myArray[i]);
+    console.log(newArray[i]);
 }
