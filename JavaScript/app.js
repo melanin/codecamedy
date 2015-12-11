6edda0fe90d1e745138e805a4738c07fe60bafd3
blkrice <blkrice123@gmail.com>
2015-12-11 23:05:47 +0900

    04 - I have to celebrate you baby

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,23 +1,41 @@
-for(var i=1; i<=20; i++)
+var getReview = function (movie)
 {
-    //console.log(i);
-    if(0 === i % 3 && 0 === i % 5)
+    /*if("Toy Story 2" == movie)
     {
-        console.log("FizzBuzz");
+        return "Great story. Mean prospector.";
+    }
+    else if("Finding Nemo" == movie)
+    {
+        return "Cool animation, and funny turtles.";
+    }
+    else if("The Lion King" == movie)
+    {
+        return "Great songs.";
     }
     else
     {
-        if(0 === i % 3)
-        {
-            console.log("Fizz");
-        }
-        else if(0 === i % 5)
-        {
-            console.log("Buzz");
-        }
-        else
-        {
-            console.log(i);
-        }
+        return "I don't know!";
+    }*/
+    
+    switch(movie)
+    {
+        case "Toy Story 2":
+            return "Great story. Mean prospector.";
+            
+        case "Finding Nemo":
+            return "Cool animation, and funny turtles.";
+            
+        case "The Lion King":
+            return "Great songs.";
+            
+        default:
+            return "I don't know!";
     }
-}
+};
+
+console.log(getReview("Toy Story 2"));
+console.log(getReview("Finding Nemo"));
+console.log(getReview("The Lion King"));
+console.log(getReview("Old Boy"));
+
+
