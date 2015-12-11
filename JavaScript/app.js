7ffafd5d951eea4cbb1e674a7f263d0c92025159
blkrice <blkrice123@gmail.com>
2015-12-11 23:34:46 +0900

    11 - Putting it all together

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,14 +1,16 @@
-// Our bob object again, but made using a constructor this time 
-var bob = new Object();
-bob.name = "Bob Smith";
-bob.age = 30;
-// Here is susan1, in literal notation
-var susan1 = {
-  name: "Susan Jordan",
-  age: 24
+// help us make snoopy using literal notation
+// Remember snoopy is a "beagle" and is 10 years old.
+var snoopy = new Object();
+snoopy.species = "beagle";
+snoopy.age = 10;
+
+// help make buddy using constructor notation
+// buddy is a "golden retriever" and is 5 years old
+var buddy =
+{
+    species: "golden retriever"
+    , age: 5
 };
-// Make a new susan2 object, using a constructor instead
-var susan2 = new Object();
-susan2.name = susan1.name;
-susan2.age = susan1.age;
-console.log(susan2);
+
+console.log(snoopy);
+console.log(buddy);
