4f6cdacbf3b0e0a82c095bb3f51c9b132f837f66
blkrice <blkrice123@gmail.com>
2015-12-11 23:12:43 +0900

    07 - Properties

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,14 @@
-var bob =
-{
+var Spencer = {
+  age: 22,
+  country: "United States"
 };
+
+// make your own object here called me
+var me =
+{
+    age: 32
+    , country: "Seoul"
+};
+
+console.log(Spencer);
+console.log(me);
