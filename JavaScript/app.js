31d32278ce24b8b9f4e09c14b4ecfb0fed3f2633
blkrice <blkrice123@gmail.com>
2015-12-11 23:37:39 +0900

    12 - More Practice Making Objects

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,16 +1,8 @@
-// help us make snoopy using literal notation
-// Remember snoopy is a "beagle" and is 10 years old.
-var snoopy = new Object();
-snoopy.species = "beagle";
-snoopy.age = 10;
-
-// help make buddy using constructor notation
-// buddy is a "golden retriever" and is 5 years old
-var buddy =
+var bicycle =
 {
-    species: "golden retriever"
-    , age: 5
+    speed: 0
+    , gear: 1
+    , frame_material: "carbon fiber"
 };
 
-console.log(snoopy);
-console.log(buddy);
+console.log(bicycle);
