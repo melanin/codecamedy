911ba0433cdd396c8b80d46dae02bb80a70593c3
blkrice <blkrice123@gmail.com>
2015-12-11 23:55:36 +0900

    14 - So What's a Method?

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,14 +1,14 @@
-// Accepts a number x as input and returns its square
-var square = function (x) {
-  return x * x;
+// here is bob again, with his usual properties
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+// this time we have added a method, setAge
+bob.setAge = function (newAge){
+  bob.age = newAge;
 };
-
-// Write the function multiply below
-// It should take two parameters and return the product
-
-
-var multiply = function(x, y)
-{
-    return x * y;
-}
-console.log(multiply(7, 8));
+// here we set bob's age to 40
+bob.setAge(40);
+// bob's feeling old.  Use our method to set bob's age to 20
+console.log(bob);
+bob.setAge(20);
+console.log(bob);
