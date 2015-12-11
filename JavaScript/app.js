a2cac99ff03b82ffb113d10c9796c392ad9f922e
blkrice <blkrice123@gmail.com>
2015-12-11 22:49:54 +0900

    02 - Through the hard times...

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,8 @@
-var answer = (((3 * 90) === 270) || !(false && (!false)) || "bex".toUpperCase() === "BEX");
+// Here is an array of multiples of 8. But is it correct?
+var multiplesOfEight = [8,16,24,32,40,58];
 
+var X = 5;
+// Test to see if a number from the array is NOT a true
+// multiple of eight. Real multiples will return false.
+var answer = multiplesOfEight[X] % 8 !== 0;
 console.log(answer);
