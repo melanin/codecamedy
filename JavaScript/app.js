011ab48b6eafe0a1dd02f7fd50a7ad1c574276f8
blkrice <blkrice123@gmail.com>
2015-12-11 23:29:46 +0900

    10 - Another Way to Create

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,14 +1,14 @@
-// Take a look at our next example object, a dog
-var dog = {
-  species: "greyhound",
-  weight: 60,
-  age: 4
+// Our bob object again, but made using a constructor this time 
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+// Here is susan1, in literal notation
+var susan1 = {
+  name: "Susan Jordan",
+  age: 24
 };
-
-var species = dog["species"];
-// fill in the code to save the weight and age using bracket notation
-var weight = dog["weight"];
-var age = dog["age"];
-
-
-console.log(species + " , " + weight + " , " + age);
+// Make a new susan2 object, using a constructor instead
+var susan2 = new Object();
+susan2.name = susan1.name;
+susan2.age = susan1.age;
+console.log(susan2);
