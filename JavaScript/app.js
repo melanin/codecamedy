1f81c4c9ce9d17b727f60097cd7315deae72d6b5
blkrice <blkrice123@gmail.com>
2015-12-11 23:15:09 +0900

    08 - Accessing Properties

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,14 +1,21 @@
-var Spencer = {
-  age: 22,
-  country: "United States"
+var bob = {
+  name: "Bob Smith",
+  age: 30
 };
-
-// make your own object here called me
-var me =
-{
-    age: 32
-    , country: "Seoul"
+var susan = {
+  name: "Susan Jordan",
+  age: 25
 };
+// here we save Bob's information
+var name1 = bob.name;
+var age1 = bob.age;
+// finish this code by saving Susan's information
+var name2 = susan.name;
+var age2 = susan.age;
 
-console.log(Spencer);
-console.log(me);
+
+console.log(name1);
+console.log(age1);
+
+console.log(name2);
+console.log(age2);
