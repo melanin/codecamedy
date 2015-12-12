13add6259465da05bdbcdfd60e4e534bfe0003d7
blkrice <blkrice123@gmail.com>
2015-12-12 17:13:58 +0900

    01 - Digitizing People ( Lesson: Building an Address Book )

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,16 +1,10 @@
-function Circle (radius) {
-    this.radius = radius;
-    this.area = function () {
-        return Math.PI * this.radius * this.radius;
-        
-    };
-    // define a perimeter method here
-    this.perimeter = function()
-    {
-        return Math.PI * (this.radius * 2);
-    };
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-777",
+    email: "bob.jones@example.com"
 };
 
-var circle = new Circle(3);
-console.log(circle.area());
-console.log(circle.perimeter());
+console.log(bob.firstName);
+console.log(bob.lastName);
+console.log(bob.email);
