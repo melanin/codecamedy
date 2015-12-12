f5a9323dad5a0300912833d37b29fd58f98fb66c
blkrice <blkrice123@gmail.com>
2015-12-12 13:24:44 +0900

    25 - Constructors in Review

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,18 +1,16 @@
-function Rectangle(height, width) {
-  this.height = height;
-  this.width = width;
-  this.calcArea = function() {
-      return this.height * this.width;
-  };
-  // put our perimeter function here!
-  this.calcPerimeter = function()
-  {
-      return (2 * this.width) + (2 * this.height);
-  }
+function Rabbit(adjective) {
+    this.adjective = adjective;
+    this.describeMyself = function() {
+        console.log("I am a " + this.adjective + " rabbit");
+    };
 }
 
-var rex = new Rectangle(7,3);
-var area = rex.calcArea();
-var perimeter = rex.calcPerimeter();
+// now we can easily make all of our rabbits
 
-console.log("area : " + area + ", perimeter : " + perimeter);
+var rabbit1 = new Rabbit("fluffy");
+var rabbit2 = new Rabbit("happy");
+var rabbit3 = new Rabbit("sleepy");
+
+rabbit1.describeMyself();
+rabbit2.describeMyself();
+rabbit3.describeMyself();
