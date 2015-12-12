5e4165521e4477d257991b2dba23d63c87932c16
blkrice <blkrice123@gmail.com>
2015-12-12 12:45:46 +0900

    21 - Custom Constructors

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,14 @@
-// here we make bob using the Object constructor
-var bob = new Object();
-bob.name = "Bob Smith";
-// add bob's age here and set it equal to 20
-bob.age = 20;
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
 
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
 console.log(bob);
+console.log(susan);
+
+var george = new Person("George Washington", 275);
+console.log(george);
