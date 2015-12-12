f62b2639ec114fb37d439fc5c6e57f39b19c99ed
blkrice <blkrice123@gmail.com>
2015-12-12 17:05:58 +0900

    31 - Properties

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,10 +1,12 @@
-var spencer = {
-  age: 22,
-  country: "United States"
-};
+var snoopy = new Object();
+snoopy.species = "beagle";
+snoopy.age = 10;
 
-// make spencer2 here with constructor notation
-var spencer2 = new Object();
-spencer2.age = spencer.age;
-spencer2.country = spencer.country;
-console.log(spencer2);
+// save Snoopy's age and species into variables
+// use dot notation for snoopy's species
+var species = snoopy.species;
+    
+// use bracket notation for snoopy's age
+var age = snoopy["age"];
+
+console.log("species : " + species + ", age : " + age);
