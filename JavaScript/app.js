601fc9ab58248557c1200b55add81d3e0b729c8a
blkrice <blkrice123@gmail.com>
2015-12-12 16:55:34 +0900

    28 - Passing Objects into Functions

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,19 +1,18 @@
-// Our Person constructor
-function Person(__name, __age)
-{
-    this.name = __name;
-    this.age = __age;
-};
-
-// Now we can make an array of people
-var family = [];
-family[0] = new Person("alice", 40);
-family[1] = new Person("bob", 42);
-family[2] = new Person("michelle", 8);
-family[3] = new Person("timmy", 6);
-
-// loop through our new array
-for(var name in family)
-{    
-    console.log(family[name]);
+// Our person constructor
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
 }
+
+// We can make a function which takes persons as arguments
+// This one computes the difference in ages between two people
+var ageDifference = function(person1, person2) {
+    return person1.age - person2.age;
+}
+
+var alice = new Person("Alice", 30);
+var billy = new Person("Billy", 25);
+
+// get the difference in age between alice and billy using our function
+var diff = ageDifference(alice, billy);
+console.log(diff);
