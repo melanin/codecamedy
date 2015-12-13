3b057e59d7dfb287d5ec5eb488a83942881ff864
blkrice <blkrice123@gmail.com>
2015-12-13 16:54:51 +0900

    03 - Displaying People

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -5,13 +5,19 @@ var bob = {
     email: "bob.jones@example.com"
 };
 
-var mary =
-{
-    firstName: "Mary"
-    , lastName: "Johnson"
-    , phoneNumber: "(650) 888 - 8888"
-    , email: "mary.johnson@example.com"
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
 };
 
-var contacts = [ bob, mary ];
-console.log(contacts[1].phoneNumber);
+var contacts = [bob, mary];
+
+// printPerson added here
+var printPerson = function(person)
+{
+    console.log(person.firstName + " " + person.lastName);
+}
+printPerson(contacts[0]);
+printPerson(contacts[1]);
