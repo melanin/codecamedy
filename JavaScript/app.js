c0101e1727c27d32cc1b501ffee678049934a0f9
blkrice <blkrice123@gmail.com>
2015-12-14 21:27:32 +0900

    17 - Marching Penguins

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,20 +1,24 @@
-// create your Animal class here
-function Animal(name, numLegs)
-{
+function Animal(name, numLegs) {
     this.name = name;
     this.numLegs = numLegs;
+}
+Animal.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
 };
 
 
-// create the sayName method for Animal
-Animal.prototype.sayName = function()
+// create a Penguin constructor here
+function Penguin(name, numLegs)
 {
-    console.log("Hi my name is " + this.name);
+    this.name = name;
+    this.numLegs = numLegs;
 }
 
+// create a sayName method for Penguins here
+Penguin.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
+};
 
-
-
-// provided code to test above constructor and method
-var penguin = new Animal("Captain Cook", 2);
-penguin.sayName();
+// our test code
+var theCaptain = new Penguin("Captain Cook", 2);
+theCaptain.sayName();
