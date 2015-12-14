d9d0001373ccff6925734e210e3cc6e636077bc4
blkrice <blkrice123@gmail.com>
2015-12-15 00:51:42 +0900

    27 - Looks For-In To Me

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,22 +1,16 @@
-function Person(first,last,age) {
-   this.firstname = first;
-   this.lastname = last;
-   this.age = age;
-   var bankBalance = 7500;
-  
-   this.askTeller = function(pass) {
-     if (pass == 1234) return bankBalance;
-     else
-        // return "Wrong password.";
-        return 0;           //문자열을 넘기면 에러가 난다
-   };
-}
+var languages = {
+    english: "Hello!",
+    french: "Bonjour!",
+    notALanguage: 4,
+    spanish: "Hola!"
+};
 
-var john = new Person('John','Smith',30);
-/* the variable myBalance should access askTeller()
-   with a password as an argument  */
-//var wrongBalance = john.askTeller(44);
-var myBalance1 = john.askTeller(44);
-var myBalance2 = john.askTeller(1234);
-console.log(myBalance1);
-console.log(myBalance2);
+// print hello in the 3 different languages
+for(var lang in languages)
+{
+    var greeting = languages[lang];
+    if("number" !== typeof greeting)
+    {
+        console.log(greeting);
+    }
+}
