70d2756ab89862b3dc6e4ee48b1c572a6d249ad1
blkrice <blkrice123@gmail.com>
2015-12-14 20:46:13 +0900

    15 - Prototype Practice

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,15 +1,19 @@
-function Dog (breed) {
-  this.breed = breed;
-};
+function Cat(name, breed) {
+    this.name = name;
+    this.breed = breed;
+}
 
-// here we make buddy and teach him how to bark
-var buddy = new Dog("golden Retriever");
-Dog.prototype.bark = function() {
-  console.log("Woof");
-};
-buddy.bark();
+// let's make some cats!
+var cheshire = new Cat("Cheshire Cat", "British Shorthair");
+var gary = new Cat("Gary", "Domestic Shorthair");
 
-// here we make snoopy
-var snoopy = new Dog("Beagle");
-/// this time it works!
-snoopy.bark();
+// add a method "meow" to the Cat class that will allow
+// all cats to print "Meow!" to the console
+Cat.prototype.meow = function()
+{
+    console.log("Meow!");
+}
+
+// add code here to make the cats meow!
+cheshire.meow();
+gary.meow();
