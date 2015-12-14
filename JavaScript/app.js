ea5837adbd85e5a3eb0685fc29029a2b4fe44069
blkrice <blkrice123@gmail.com>
2015-12-14 19:55:18 +0900

    01 - An Objective Review ( Lesson: Introduction to Objects II )

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,56 +1,17 @@
-var bob = {
-    firstName: "Bob",
-    lastName: "Jones",
-    phoneNumber: "(650) 777-7777",
-    email: "bob.jones@example.com"
+var james = {
+    // add properties to this object!
+    job: "programmer"
+    , married: false
+    
 };
 
-var mary = {
-    firstName: "Mary",
-    lastName: "Johnson",
-    phoneNumber: "(650) 888-8888",
-    email: "mary.johnson@example.com"
-};
-
-var contacts = [bob, mary];
-
-function printPerson(person) {
-    console.log(person.firstName + " " + person.lastName);
+function Person(job, married) {
+    this.job = job;
+    this.married = married;
 }
 
-function list() {
-	var contactsLength = contacts.length;
-	for (var i = 0; i < contactsLength; i++) {
-		printPerson(contacts[i]);
-	}
-}
+// create a "gabby" object using the Person constructor!
+var gabby = new Person("student", true);
 
-/*Create a search function
-then call it passing "Jones"*/
-function search(lastName)
-{
-    var contactsLength = contacts.length;
-    for(var i=0; i<contactsLength; i++)
-    {
-        if(lastName === contacts[i].lastName)
-        {
-            printPerson(contacts[i]);
-            break;
-        }
-    }
-}
-//search("Jones");
-
-function add(firstName, lastName, email, phoneNumber)
-{
-    contacts[contacts.length] =
-    {
-        firstName: firstName,
-        lastName: lastName,
-        phoneNumber: email,
-        email: phoneNumber
-    };
-}
-add("MELA", "coder", "melanin951@gmail.com", "010-3291-xxxx");
-
-list();
+console.log(james);
+console.log(gabby);
