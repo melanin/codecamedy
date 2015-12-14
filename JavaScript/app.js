8448e5528b5f42f2b447299f6fa700e89b89572f
blkrice <blkrice123@gmail.com>
2015-12-14 20:19:27 +0900

    08 - Dressed to Impress

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,14 @@
-var myObj = {
-    // finish myObj
-    name: "MELA"
+var suitcase = {
+    shirt: "Hawaiian"
 };
 
-console.log( myObj.hasOwnProperty('name') ); // should print true
-console.log( myObj.hasOwnProperty('nickname') ); // should print false
+//console.log(suitcase.shorts);
+if(suitcase.hasOwnProperty("shorts"))
+{
+    console.log(suitcase.shorts);
+}
+else
+{
+    suitcase.shorts = "short short";
+    console.log(suitcase.shorts);
+}
