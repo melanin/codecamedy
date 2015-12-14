d954c075e8d2c67eff64a1248153888e3dae5bea
blkrice <blkrice123@gmail.com>
2015-12-15 00:11:31 +0900

    26 - Passing Arguments

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -4,19 +4,19 @@ function Person(first,last,age) {
    this.age = age;
    var bankBalance = 7500;
   
-   var returnBalance = function() {
-      return bankBalance;
+   this.askTeller = function(pass) {
+     if (pass == 1234) return bankBalance;
+     else
+        // return "Wrong password.";
+        return 0;           //문자열을 넘기면 에러가 난다
    };
-       
-   // create the new function here
-   this.askTeller = function()
-   {
-       return returnBalance;
-   }
 }
 
 var john = new Person('John','Smith',30);
-console.log(john.returnBalance);
-var myBalanceMethod = john.askTeller();
-var myBalance = myBalanceMethod();
-console.log(myBalance);
+/* the variable myBalance should access askTeller()
+   with a password as an argument  */
+//var wrongBalance = john.askTeller(44);
+var myBalance1 = john.askTeller(44);
+var myBalance2 = john.askTeller(1234);
+console.log(myBalance1);
+console.log(myBalance2);
