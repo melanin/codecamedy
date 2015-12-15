e253a9772ed38abf1642f61b8258e0a3ee4743d3
blkrice <blkrice123@gmail.com>
2015-12-15 23:06:07 +0900

    02 - Manually Add It Up?

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,16 @@
-//Create the object called cashRegister 
-//and initialize its total property
-var cashRegister = new Object();
-cashRegister.total = 0;
-console.log(cashRegister.total);
+var cashRegister = {
+    total:0,
+    add: function(itemCost){
+        this.total += itemCost;
+    }
+};
 
-//Using dot notation change the total property
-cashRegister.total = 2.99;
-console.log(cashRegister.total);
+
+//call the add method for our items
+cashRegister.add(0.98);
+cashRegister.add(1.23);
+cashRegister.add(4.99);
+cashRegister.add(0.45);
+
+//Show the total bill
+console.log('Your bill is '+cashRegister.total);
