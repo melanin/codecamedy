0a64b22c30bd4fd2ce5db1c93763b25a8b3d8e82
blkrice <blkrice123@gmail.com>
2015-12-16 00:58:43 +0900

    06 - Over the Moon

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,35 +1,16 @@
-var cashRegister = {
-    total:0,
-    //Dont forget to add your property
-    add: function(itemCost) {
-        this.total +=  itemCost;
-        this.lastTransactionAmount = itemCost;
-    },
-    scan: function(item,quantity) {
-        switch (item) {
-        case "eggs": this.add(0.98 * quantity); break;
-        case "milk": this.add(1.23 * quantity); break;
-        case "magazine": this.add(4.99 * quantity); break;
-        case "chocolate": this.add(0.45 * quantity); break;
-        }
-        return true;
-    },
-    //Add the voidLastTransaction Method here
-    voidLastTransaction: function()
-    {
-        this.total -= this.lastTransactionAmount;
-    }
-};
+// create a constructor for the StaffMember class
+function StaffMember(name, discountPercent)
+{
+    this.name = name;
+    this.discountPercent = discountPercent;
+}
 
-cashRegister.scan('eggs',1);
-cashRegister.scan('milk',1);
-cashRegister.scan('magazine',1);
-cashRegister.scan('chocolate',4);
+var sally = new StaffMember("Sally",5);
+var bob = new StaffMember("Bob",10);
 
-//Void the last transaction and then add 3 instead
-cashRegister.voidLastTransaction();
-cashRegister.scan('chocolate',3);
+//Create a StaffMember for yourself called me
+var me = new StaffMember("MELA", 20);
 
-
-//Show the total bill
-console.log('Your bill is '+cashRegister.total);
+console.log(sally);
+console.log(bob);
+console.log(me);
