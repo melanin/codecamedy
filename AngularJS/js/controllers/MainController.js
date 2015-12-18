032361713f4bb4c3ffc002192e0cd847efa95ae7
blkrice <blkrice123@gmail.com>
2015-12-18 18:16:15 +0900

    09 - ng-click I

--- AngularJS/js/controllers/MainController.js
+++ AngularJS/js/controllers/MainController.js
@@ -14,25 +14,33 @@ app.controller('MainController', ['$scope', function($scope)
                                         name: 'The Book of Trees', 
                                         price: 19, 
                                         pubdate: new Date('2014', '03', '08'), 
-                                        cover: 'img/the-book-of-trees.jpg' 
+                                        cover: 'img/the-book-of-trees.jpg',
+                                        likes: 0
                                       }, 
                                       { 
                                         name: 'Program or be Programmed', 
                                         price: 8, 
                                         pubdate: new Date('2013', '08', '01'), 
-                                        cover: 'img/program-or-be-programmed.jpg' 
+                                        cover: 'img/program-or-be-programmed.jpg',
+                                        likes: 0
                                       },
                                       { 
                                         name: 'The Book of Trees', 
                                         price: 19, 
                                         pubdate: new Date('2014', '03', '08'), 
-                                        cover: 'img/the-book-of-trees.jpg' 
+                                        cover: 'img/the-book-of-trees.jpg',
+                                        likes: 0
                                       }, 
                                       { 
                                         name: 'Program or be Programmed', 
                                         price: 8, 
                                         pubdate: new Date('2013', '08', '01'), 
-                                        cover: 'img/program-or-be-programmed.jpg' 
+                                        cover: 'img/program-or-be-programmed.jpg',
+                                        likes: 0
                                       }
                                     ];
+                                    $scope.plusOne = function(index)
+                                    {
+                                      $scope.products[index].likes += 1;
+                                    };
                                   }]);
