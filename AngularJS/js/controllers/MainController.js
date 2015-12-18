35b5194c66f06e703cb56b379d5f8ed1ccdb4cf9
blkrice <blkrice123@gmail.com>
2015-12-18 17:53:01 +0900

    05 - Filters II

--- AngularJS/js/controllers/MainController.js
+++ AngularJS/js/controllers/MainController.js
@@ -1,10 +1,10 @@
-app.controller('MainController', ['$scope', function($scope)
-                                  {
+app.controller('MainController', ['$scope', function($scope) {
                                     $scope.title = 'AngularJS App';
                                     $scope.promo = 'Promo Promo';
                                     $scope.product =
-                                    {
-                                      name: 'The Book of Trees'
-                                      , price: 19
+                                    {                                      
+                                      name: 'The Book of Trees',
+                                      price: 19,
+                                      pubdate: new Date('2014', '03', '08')
                                     };
                                   }]);
