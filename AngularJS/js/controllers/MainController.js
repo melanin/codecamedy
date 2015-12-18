848db13d73f133a14f1fcd4439a517c90472b268
blkrice <blkrice123@gmail.com>
2015-12-18 17:34:45 +0900

    03 - Workflow

--- AngularJS/js/controllers/MainController.js
+++ AngularJS/js/controllers/MainController.js
@@ -2,4 +2,9 @@ app.controller('MainController', ['$scope', function($scope)
                                   {
                                     $scope.title = 'AngularJS App';
                                     $scope.promo = 'Promo Promo';
+                                    $scope.product =
+                                    {
+                                      name: 'The Book of Trees'
+                                      , price: 19
+                                    };
                                   }]);
