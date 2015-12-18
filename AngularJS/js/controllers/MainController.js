738d441b927cbe775956ee277dccd7b271412714
blkrice <blkrice123@gmail.com>
2015-12-18 23:24:43 +0900

    01 - Directives I ( Lesson: Directives )

--- AngularJS/js/controllers/MainController.js
+++ AngularJS/js/controllers/MainController.js
@@ -1,54 +1,23 @@
-app.controller('MainController', ['$scope', function($scope)
-                                  {
-                                    $scope.title = 'AngularJS App';
-                                    $scope.promo = 'Promo Promo';
-                                    /*$scope.product =
-                                    {                                      
-                                      name: 'The Book of Trees',
-                                      price: 19,
-                                      pubdate: new Date('2014', '03', '08')
-                                    };*/
-                                    $scope.products =
-                                    [
-                                      { 
-                                        name: 'The Book of Trees', 
-                                        price: 19, 
-                                        pubdate: new Date('2014', '03', '08'), 
-                                        cover: 'img/the-book-of-trees.jpg',
-                                        likes: 0,
-                                        dislikes: 0
-                                      }, 
-                                      { 
-                                        name: 'Program or be Programmed', 
-                                        price: 8, 
-                                        pubdate: new Date('2013', '08', '01'), 
-                                        cover: 'img/program-or-be-programmed.jpg',
-                                        likes: 0,
-                                        dislikes: 0
-                                      },
-                                      { 
-                                        name: 'The Book of Trees', 
-                                        price: 19, 
-                                        pubdate: new Date('2014', '03', '08'), 
-                                        cover: 'img/the-book-of-trees.jpg',
-                                        likes: 0,
-                                        dislikes: 0
-                                      }, 
-                                      { 
-                                        name: 'Program or be Programmed', 
-                                        price: 8, 
-                                        pubdate: new Date('2013', '08', '01'), 
-                                        cover: 'img/program-or-be-programmed.jpg',
-                                        likes: 0,
-                                        dislikes: 0
-                                      }
-                                    ];
-                                    $scope.plusOne = function(index)
-                                    {
-                                      $scope.products[index].likes += 1;
-                                    };
-                                    $scope.minusOne = function(index)
-                                    {
-                                      $scope.products[index].dislikes += 1;
-                                    };
-                                  }]);
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.move = {
+    icon: 'img/move.jpg',
+    title: 'MOVE',
+    developer: 'MOVE, Inc.',
+    price: 0.99
+  };
+
+  $scope.shutterbugg = {
+    icon: 'img/shutterbugg.jpg',
+    title: 'Shutterbugg',
+    developer: 'Chico Dusty',
+    price: 2.99
+  };
+
+  $scope.gameboard = {
+    icon: 'img/gameboard.jpg',
+    title: 'Gameboard',
+    developer: 'Armando P.',
+    price: 1.99
+  };
+}]);
+Open
